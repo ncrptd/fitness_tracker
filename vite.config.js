2c9720828523d4f47945337d708052344d76589d
Rockey Biswas <rockeybiswas592@gmail.com>
2023-10-06 20:24:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { createServer } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+})
+
+createServer({
+  root: './dist',
+  server: {
+    middleware: async (req, res, next) => {
+      if (req.url !== '/') {
+        res.writeHead(302, { Location: '/' });
+        res.end();
+        return;
+      }
+      next();
+    },
+  },
 })
